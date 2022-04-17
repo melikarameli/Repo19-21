c9d38768dd760381088097aa6ef3117ba7a43b57
melikarameli <melikahatunic2001@gmail.com>
2022-04-17 13:24:51 +0200

    Added Keyboard

---
+++ ex01/src/index.js
@@ -0,0 +1,155 @@
+function myFunction() {
+    var one = document.getElementById("btn1").value;
+    document.getElementById("keyboardInput").innerHTML = one;
+}
+function myFunction2() {
+    var two = document.getElementById("btn2").value;
+    document.getElementById("keyboardInput").innerHTML = two;
+}
+function myFunction3() {
+    var three = document.getElementById("btn3").value;
+    document.getElementById("keyboardInput").innerHTML = three;
+}
+function myFunction4() {
+    var four = document.getElementById("btn4").value;
+    document.getElementById("keyboardInput").innerHTML = four;
+}
+function myFunction5() {
+    var five = document.getElementById("btn5").value;
+    document.getElementById("keyboardInput").innerHTML = five;
+}
+function myFunction6() {
+    var six = document.getElementById("btn6").value;
+    document.getElementById("keyboardInput").innerHTML = six;
+}
+function myFunction7() {
+    var seven = document.getElementById("btn7").value;
+    document.getElementById("keyboardInput").innerHTML = seven;
+}
+function myFunction8() {
+    var eight = document.getElementById("btn8").value;
+    document.getElementById("keyboardInput").innerHTML = eight;
+}
+function myFunction9() {
+    var nine = document.getElementById("btn9").value;
+    document.getElementById("keyboardInput").innerHTML = nine;
+}
+function myFunction0() {
+    var zero = document.getElementById("btn0").value;
+    document.getElementById("keyboardInput").innerHTML = zero;
+}
+function myFunctionBs() {
+    document.getElementById("keyboard").removeAttribute("value"); 
+}
+function myFunctionq() {
+    var q = document.getElementById("btnq").value;
+    document.getElementById("keyboardInput").innerHTML = q;
+}
+function myFunctionw() {
+    var w = document.getElementById("btnw").value;
+    document.getElementById("keyboardInput").innerHTML = w;
+}
+function myFunctione() {
+    var e = document.getElementById("btne").value;
+    document.getElementById("keyboardInput").innerHTML = e;
+}
+function myFunctionr() {
+    var r = document.getElementById("btnr").value;
+    document.getElementById("keyboardInput").innerHTML = r;
+}
+function myFunctiont() {
+    var t = document.getElementById("btnt").value;
+    document.getElementById("keyboardInput").innerHTML = t;
+}
+function myFunctiony() {
+    var y = document.getElementById("btny").value;
+    document.getElementById("keyboardInput").innerHTML = y;
+}
+function myFunctionu() {
+    var u = document.getElementById("btnu").value;
+    document.getElementById("keyboardInput").innerHTML = u;
+}
+function myFunctioni() {
+    var i = document.getElementById("btni").value;
+    document.getElementById("keyboardInput").innerHTML = i;
+}
+function myFunctiono() {
+    var o = document.getElementById("btno").value;
+    document.getElementById("keyboardInput").innerHTML = o;
+}
+function myFunctionp() {
+    var p = document.getElementById("btnp").value;
+    document.getElementById("keyboardInput").innerHTML = p;
+}
+function myFunctiona() {
+    var a = document.getElementById("btna").value;
+    document.getElementById("keyboardInput").innerHTML = a;
+}
+function myFunctions() {
+    var s = document.getElementById("btns").value;
+    document.getElementById("keyboardInput").innerHTML = s;
+}
+function myFunctiond() {
+    var d = document.getElementById("btnd").value;
+    document.getElementById("keyboardInput").innerHTML = d;
+}
+function myFunctionf() {
+    var f = document.getElementById("btnf").value;
+    document.getElementById("keyboardInput").innerHTML = f;
+}
+function myFunctiong() {
+    var g = document.getElementById("btng").value;
+    document.getElementById("keyboardInput").innerHTML = g;
+}
+function myFunctionh() {
+    var h = document.getElementById("btnh").value;
+    document.getElementById("keyboardInput").innerHTML = h;
+}
+function myFunctionj() {
+    var j = document.getElementById("btnj").value;
+    document.getElementById("keyboardInput").innerHTML = j;
+}
+function myFunctionk() {
+    var k = document.getElementById("btnk").value;
+    document.getElementById("keyboardInput").innerHTML = k;
+}
+function myFunctionl() {
+    var l = document.getElementById("btnl").value;
+    document.getElementById("keyboardInput").innerHTML = l;
+}
+function myFunctionz() {
+    var z = document.getElementById("btnz").value;
+    document.getElementById("keyboardInput").innerHTML = z;
+}
+function myFunctionx() {
+    var x = document.getElementById("btnx").value;
+    document.getElementById("keyboardInput").innerHTML = x;
+}
+function myFunctionc() {
+    var c = document.getElementById("btnc").value;
+    document.getElementById("keyboardInput").innerHTML = c;
+}
+function myFunctionv() {
+    var v = document.getElementById("btnv").value;
+    document.getElementById("keyboardInput").innerHTML = v;
+}
+function myFunctionb() {
+    var b = document.getElementById("btnb").value;
+    document.getElementById("keyboardInput").innerHTML = b;
+}
+function myFunctionn() {
+    var n = document.getElementById("btnn").value;
+    document.getElementById("keyboardInput").innerHTML = n;
+}
+function myFunctionm() {
+    var m = document.getElementById("btnm").value;
+    document.getElementById("keyboardInput").innerHTML = m;
+}
+function myFunctionC() {
+    var cr = document.getElementById("btn-").value;
+    document.getElementById("keyboardInput").innerHTML = cr;
+}
+function myFunctionDc() {
+    var Dc = document.getElementById("btn_").value;
+    document.getElementById("keyboardInput").innerHTML = Dc;
+}
